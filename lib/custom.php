25ace3e0490d0a40572f098bd63c915c7fa3a663
michael <michael@12southmusic.com>
2014-05-13 17:07:16 -0500

    Create post image shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -2,4 +2,22 @@
 
 define( 'UPLOADS', ''.'assets' );
 
+/ Get URL of first image in a post
+function catch_that_image($atts) {
+ extract( shortcode_atts( array(
+    'id' => '',
+    ), $atts ) );
+global $post, $posts;
+$post = get_post($id);
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+$first_img = $matches [1] [0];
+
+return $first_img;
+}
+
+add_shortcode('post-image','catch_that_image');
+
 ?>
