d2931d321a2b3af7e5c7ce5472c19e998952a313
michael <michael@12southmusic.com>
2014-05-13 14:56:31 -0500

    Move home nag into something else

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,3 +1,32 @@
+<div class="row row-slider">
+	<div class="col-sm-12">
+		<?php echo do_shortcode('[layerslider id="1"]'); ?>
+	</div>
+</div>
+
+<header class="banner navbar navbar-default affix" role="banner" data-spy="affix" data-offset-top="60" data-offset-bottom="200">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
+  </div>
+</header>
+
+
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
