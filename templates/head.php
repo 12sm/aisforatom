2524564f6e0ff17fb2ada941513f2411cd75c6e3
michael <michael@12southmusic.com>
2014-05-13 12:20:37 -0500

    Push fonts and bg images

--- templates/head.php
+++ templates/head.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html><!-- 
-   _      ___    ____                
- /' \   /'___`\ /\  _`\   /'\_/`\    
-/\_, \ /\_\ /\ \\ \,\L\_\/\      \   
-\/_/\ \\/_/// /__\/_\__ \\ \ \__\ \  
-   \ \ \  // /_\ \ /\ \L\ \ \ \_/\ \ 
-    \ \_\/\______/ \ `\____\ \_\\ \_\
-     \/_/\/_____/   \/_____/\/_/ \/_/                                                                                                           
- 
+<!DOCTYPE html> <![CDATA[
+|   _      ___    ____
+| /' \   /'___`\ /\  _`\   /'\_/`\
+|/\_, \ /\_\ /\ \\ \,\L\_\/\      \
+|\/_/\ \\/_/// /__\/_\__ \\ \ \__\ \
+|   \ \ \  // /_\ \ /\ \L\ \ \ \_/\ \
+|    \ \_\/\______/ \ `\____\ \_\\ \_\
+|     \/_/\/_____/   \/_____/\/_/ \/_/
 built by 12South Music
 http://12southmusic.com/
- 
- -->
+]]>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
@@ -23,6 +21,9 @@ http://12southmusic.com/
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
+
+  <script type="text/javascript" src="//use.typekit.net/okq5cym.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
