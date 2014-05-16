c9966a873a2022b5f48f2fac64b4372ebcef3f09
Sarah <Sarah@12southmusic.com>
2014-05-16 11:27:58 -0500

    col in sidebar

--- base.php
+++ base.php
@@ -21,8 +21,10 @@
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar col <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
+        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+			<div class="col">
+          		<?php include roots_sidebar_path(); ?>
+		    </div>
         </aside><!-- /.sidebar -->
       <?php endif; ?>
     </div><!-- /.content -->
