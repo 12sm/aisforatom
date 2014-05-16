8aa76be32fbfbb670d7802f3b2a063e43f9b1c3f
michael <michael@12southmusic.com>
2014-05-16 10:35:06 -0500

    Change order of masonry sort

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,13 +36,32 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on the home page
       
-      var $container = $('.masonry');
+      /* var $container = $('.masonry');
 
         $container.masonry({
             itemSelector: '.blog-item',
             columnWidth: '.blog-item',
             transitionDuration: 0
-        });
+        }); */
+        
+        var ar = $('.masonry').children();
+        ar.sort(function(a,b){
+	        // Get a random number between 0 and 10
+	        var temp = parseInt( Math.random()*10 );
+	        // Get 1 or 0, whether temp is odd or even
+	        var isOddOrEven = temp%2;
+	        // Get +1 or -1, whether temp greater or smaller than 5
+	        var isPosOrNeg = temp>5 ? 1 : -1;
+	        // Return -1, 0, or +1
+	        return( isOddOrEven*isPosOrNeg );
+	    });
+	    $('.masonry').html(ar);
+	    $('.masonry').masonry({ 
+            itemSelector: '.blog-item',
+            columnWidth: '.blog-item',
+            transitionDuration: 0
+            animate: true
+         });
       
     }
   },
