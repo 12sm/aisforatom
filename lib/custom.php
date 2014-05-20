8fb098cf6fb8e5cf62653a1cf24a210efafd8d9f
michael <michael@12southmusic.com>
2014-05-20 18:06:31 -0500

    New custom function

--- lib/custom.php
+++ lib/custom.php
@@ -20,4 +20,23 @@ return $first_img;
 
 add_shortcode('post-image','catch_that_image');
 
+
+// Get URL of first image in a post
+function print_that_image($atts) {
+ extract( shortcode_atts( array(
+    'id' => '',
+    ), $atts ) );
+global $post, $posts;
+$post = get_post($id);
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+$first_img = "<img src='".$matches [1] [0]."' />";
+
+return $first_img;
+}
+
+add_shortcode('print-image','print_that_image');
+
 ?>
