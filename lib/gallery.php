5534fc5cc20ce9b7b9363cd4e9b61497161949cc
michael <michael@12southmusic.com>
2014-05-21 14:26:21 -0500

    Thumbnail versus thumb

--- lib/gallery.php
+++ lib/gallery.php
@@ -41,7 +41,7 @@ function roots_gallery($attr) {
     'icontag'    => '',
     'captiontag' => '',
     'columns'    => 4,
-    'size'       => 'thumb',
+    'size'       => 'thumbnail',
     'include'    => '',
     'exclude'    => '',
     'link'       => ''
